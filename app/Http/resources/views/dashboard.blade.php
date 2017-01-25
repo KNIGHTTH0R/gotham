b73f5579ddc202dc88b10a2c498ecd8821678d84
James Muldrow <jamesmuldrow@gmail.com>
2017-01-25 07:25:39 -0500

    stuff

--- app/Http/resources/views/dashboard.blade.php
+++ app/Http/resources/views/dashboard.blade.php
@@ -4,17 +4,20 @@ use gotham\Http\Controllers\MyUtilController;
 @extends('templates.dashboard_template')
 
 @section('content')
-    <div>
-        @if(Auth::check())
-            <?php
-                $user = Auth::user();
-                $myutil = new MyUtilController;
-            ?>
-        @endif
-        <h1>Welcome, {{ $myutil->firstlettertoupper($user->first_name) }}</h1>
+    <div class="row" style="margin:25px;" >
+        <div>
+            @if(Auth::check())
+                <?php
+                    $user = Auth::user();
+                    $myutil = new MyUtilController;
+                ?>
+            @endif
+            <h1 style="margin-top: 0;">Welcome, {{ $myutil->firstlettertoupper($user->first_name) }}</h1>
+        </div>
+        <form method="POST" action="{{url('/logout')}}">
+            {{ csrf_field() }}
+            <input type="submit" class="glyphicon glyphicon-off"/>
+            <span class="glyphicon glyphicon-off"></span>
+        </form>
     </div>
-    <form method="POST" action="{{url('/logout')}}">
-        {{ csrf_field() }}
-        <input type="submit" class="btn btn-primary" value="logout"/>
-    </form>
 @endsection
