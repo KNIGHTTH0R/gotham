44d67d06a6e2eb160c931d7440e1589e220437c0
James Muldrow <jamesmuldrow@gmail.com>
2017-02-06 07:28:54 -0500

    Updated collaborators section

--- app/Http/resources/views/projects/projects_show.blade.php
+++ app/Http/resources/views/projects/projects_show.blade.php
@@ -18,71 +18,82 @@
         <?php   
             $colspan = 3;
         ?>
-        <div style="background-color:#2c2c2c; padding:5px; margin-right: 10px" class="col-md-8">
+        <div style="margin-bottom: 10px; background-color:#2c2c2c; padding:5px; " class="col-md-11">
             <table style="width:100%">
                 <th colspan="{{$colspan}}" style="padding:10px;border-bottom:3px solid #5f4a3d;background-color: #212121">
                         Project: {{ $project->name }}</th>
                 <tr><td>Description: {{ $project->description }}</td></tr>
             </table>
         </div>
-        <div style="background-color:#2c2c2c; padding:5px;" class="col-md-3">
-            <table style="width:100%">
-                <th colspan="{{$colspan}}" style="padding:10px;border-bottom:3px solid #5f4a3d;background-color: #212121">
-                        Collaborators</th>
-                <!--Finish this section later!!! You need to add collaborators to project page-->
-            </table>
-        </div>
-    </div>
-    <div class="row" style="margin:25px; margin-bottom: 200px;padding-right:10px">
-        
-        <div style="margin-right:10px; background-color:#2c2c2c; padding:5px;" class="col-md-5">
-            <table style="width:100%">
-                <th colspan="{{$colspan}}" 
+
+        <div style="margin-right: 2px; background-color:#2c2c2c;padding: 5px;" class="col-md-8">
+            <table style="width:100%; ">
+                <th colspan="{{$colspan}}"
                     style="padding:10px;border-bottom:3px solid #5f4a3d;background-color: #212121">
                     Your RFI's
                 </th>
-                
+
                 <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
-                    <th style="padding-left:10px;border-bottom:1px solid #5f4a3d;background-color: #212121">Subject</th>
-                    <!--<th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Submitted by</th>-->
-                    <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Last update</th>
-                    <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
+                <th style="padding-left:10px;border-bottom:1px solid #5f4a3d;background-color: #212121">Subject</th>
+                <!--<th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Submitted by</th>-->
+                <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Last update</th>
+                <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
                 </tr>
                 <tr>
-                @foreach($project->rfis()->get() as $rfi)
-                    @if($rfi->user_id == Auth::id())
-                        <td style="padding:3px;">{{ $rfi->subject }}</td>
+                    @foreach($project->rfis()->get() as $rfi)
+                        @if($rfi->user_id == Auth::id())
+                            <td style="padding:3px;">{{ $rfi->subject }}</td>
                         <!--<td>{{ gotham\User::find($rfi->user_id)->first_name }}</td>-->
-                        <td>{{ $rfi->updated_at }}</td>
-                    @endif
+                            <td>{{ $rfi->updated_at }}</td>
+                        @endif
                 </tr>
                 @endforeach
             </table>
         </div>
-        <div style="padding:0; background-color:#2c2c2c; padding:5px;" class="col-md-6">
+        <div style="background-color:#2c2c2c; padding:5px;" class="col-md-3">
             <table style="width:100%">
-                <th colspan="{{$colspan}}" 
+                <th colspan="{{$colspan}}" style="padding:10px;border-bottom:3px solid #5f4a3d;background-color: #212121">
+                    Collaborators</th>
+                <!--- Finish this section later!!! You need to add collaborators to project page-->
+                <!--- Add Collaborators by saving User to $project->user()->save($user), any other
+                      way will provide unexpected results --->
+                @foreach(gotham\Project::find($project->id)->users as $collaborators)
+                    <tr><td>{{ $collaborators->first_name }}</td></tr>
+                @endforeach
+            </table>
+        </div>
+
+        <div style="padding:0; background-color:#2c2c2c; padding:5px;margin-top: 10px;" class="col-md-8">
+            <table style="width:100%">
+                <th colspan="{{$colspan}}"
                     style="padding:10px;border-bottom:3px solid #5f4a3d;background-color: #212121">
                     All Other RFI's
                 </th>
-                
+
                 <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
-                    <th style="padding-left:10px;border-bottom:1px solid #5f4a3d;background-color: #212121">Subject</th>
-                    <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Submitted by</th>
-                    <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Last update</th>
-                    <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
+                <th style="padding-left:10px;border-bottom:1px solid #5f4a3d;background-color: #212121">Subject</th>
+                <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Submitted by</th>
+                <th style="border-bottom:1px solid #5f4a3d;background-color: #212121">Last update</th>
+                <tr><td colspan="{{$colspan}}"><hr style="margin:0; border-color:#5f4a3d"></td></tr>
                 </tr>
                 <tr>
-                @foreach($project->rfis()->get() as $rfi)
-                    @if($rfi->user_id != Auth::id())
-                        <td style="padding:3px;">{{ $rfi->subject }}</td>
-                        <td>{{ gotham\User::find($rfi->user_id)->first_name }}</td>
-                        <td>{{ $rfi->updated_at }}</td>
-                    @endif
+                    @foreach($project->rfis()->get() as $rfi)
+                        @if($rfi->user_id != Auth::id())
+                            <td style="padding:3px;">{{ $rfi->subject }}</td>
+                            <td>{{ gotham\User::find($rfi->user_id)->first_name }}</td>
+                            <td>{{ $rfi->updated_at }}</td>
+                        @endif
                 </tr>
                 @endforeach
             </table>
         </div>
+
+    </div>
+
+    <div class="row" style="margin:25px; margin-bottom: 200px;padding-right:10px">
+        
+
+
     </div>
     
 @endsection
